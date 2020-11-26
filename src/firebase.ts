baf2463b1779e1844680c7f730f58e10525675a6
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2020-11-25 22:28:39 -0600

    ADD Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: 'AIzaSyBlW3vO9e2knuaaB-Qb07ux4aDPOyeJw2E',
+    authDomain: 'nullpointer-dc5e2.firebaseapp.com',
+    databaseURL: 'https://nullpointer-dc5e2.firebaseio.com',
+    projectId: 'nullpointer-dc5e2',
+    storageBucket: 'nullpointer-dc5e2.appspot.com',
+    messagingSenderId: '204712167391',
+    appId: '1:204712167391:web:0fb3bd4e4de0b0a527e66a',
+};
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+export const db = fb.firestore();
