321aa73515b459e121891fb24ba85b1fdf9a3555
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2020-11-26 19:23:17 -0600

    UPDATE Registro Component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,83 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Input, Button, Checkbox, Card } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
-import '../index.css';
 
-const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-};
-export default class Login extends Component {
-    render() {
-        return (
-            <div className="login-container">
-                <Card title="Inicia Sesión" className="card">
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
+export const Login = () => {
+    const onFinish = (values: any) => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <div className="login-container">
+            <Card title="Inicia Sesión" className="card">
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Ingresa tu Correo',
+                            },
+                        ]}
                     >
+                        <Input
+                            prefix={
+                                <UserOutlined className="site-form-item-icon" />
+                            }
+                            placeholder="Correo"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Ingresa tu Contraseña',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={
+                                <LockOutlined className="site-form-item-icon" />
+                            }
+                            type="password"
+                            placeholder="Contraseña"
+                        />
+                    </Form.Item>
+                    <Form.Item>
                         <Form.Item
-                            name="email"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Ingresa tu Correo',
-                                },
-                            ]}
+                            name="remember"
+                            valuePropName="checked"
+                            noStyle
                         >
-                            <Input
-                                prefix={
-                                    <UserOutlined className="site-form-item-icon" />
-                                }
-                                placeholder="Correo"
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Ingresa tu Contraseña',
-                                },
-                            ]}
-                        >
-                            <Input
-                                prefix={
-                                    <LockOutlined className="site-form-item-icon" />
-                                }
-                                type="password"
-                                placeholder="Contraseña"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Form.Item
-                                name="remember"
-                                valuePropName="checked"
-                                noStyle
-                            >
-                                <Checkbox>Recordarme</Checkbox>
-                            </Form.Item>
-
-                            <a className="login-form-forgot" href="">
-                                Olvidé mi contraseña
-                            </a>
+                            <Checkbox>Recordarme</Checkbox>
                         </Form.Item>
 
-                        <Form.Item>
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                className="login-form-button"
-                            >
-                                Iniciar Sesión
-                            </Button>
-                            O <Link to="/registro">Registrarse</Link>
-                        </Form.Item>
-                    </Form>
-                </Card>
-            </div>
-        );
-    }
-}
+                        <a className="login-form-forgot" href="">
+                            Olvidé mi contraseña
+                        </a>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className="login-form-button"
+                        >
+                            Iniciar Sesión
+                        </Button>
+                        O <Link to="/registro">Registrarse</Link>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    );
+};
