84689755d7ab82fd6117942c2324e4c8cc428154
Daniel Garnica Sánchez <thedaniel115@gmail.com>
2020-11-29 15:26:05 -0600

    ADD Ver Cuentas

--- src/components/AdminCuentas.tsx
+++ src/components/AdminCuentas.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFirebase } from '../contexts/FirebaseContext';
 import { TarjetaCuenta } from './TarjetaCuenta';
 
 export const AdminCuentas = () => {
+    const { usuarioM } = useFirebase();
+    const [usuarios, setUsuarios] = useState<Usuario[]>();
+    usuarioM.subscribe(setUsuarios, usuarioM.getCollection());
     return (
         <div>
-            <TarjetaCuenta />
+            {usuarios?.map((usuario) => {
+                return <TarjetaCuenta key={usuario.id} usuario={usuario} />;
+            })}
+            {/* <TarjetaCuenta /> */}
         </div>
     );
 };
--- src/components/TarjetaCuenta.tsx
+++ src/components/TarjetaCuenta.tsx
@@ -1,35 +1,78 @@
-import { Card, Avatar, Switch } from 'antd';
-import {
-    EditOutlined,
-    EllipsisOutlined,
-    SettingOutlined,
-} from '@ant-design/icons';
-import React from 'react';
+import { Card, Avatar, Switch, Modal, Button } from 'antd';
+import { EllipsisOutlined, DeleteOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
 const { Meta } = Card;
 
-export const TarjetaCuenta = () => {
-    function onChange(checked: any) {
-        console.log(`switch to ${checked}`);
-    }
+export const TarjetaCuenta = ({ usuario }: any) => {
+    const [state, setState] = useState({ visible: false });
+    const { nombre, correo, isAdmin } = usuario;
+
+    const onChange = (checked: any) => {
+        showModal();
+    };
+
+    const onDelete = () => {
+        console.log('Delete');
+    };
+
+    const onDetails = () => {
+        console.log('Detail');
+        console.log(usuario);
+    };
+
+    const showModal = () => {
+        setState({
+            visible: true,
+        });
+    };
+
+    const handleOk = (e: any) => {
+        console.log(e);
+        setState({
+            visible: false,
+        });
+    };
+
+    const handleCancel = (e: any) => {
+        console.log(e);
+        setState({
+            visible: false,
+        });
+    };
 
     return (
         <Card
             style={{ width: 300, marginTop: 16 }}
             actions={[
-                <SettingOutlined key="setting" />,
-                <EllipsisOutlined key="ellipsis" />,
+                <DeleteOutlined key="delete" onClick={onDelete} />,
+                <EllipsisOutlined key="ellipsis" onClick={onDetails} />,
             ]}
         >
             <Meta
                 avatar={
                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                 }
-                title="Nombre Usuaro"
-                description="Correo"
+                title={nombre}
+                description={correo}
             />
-            {/* <p>
-                Administrador: <Switch defaultChecked onChange={onChange} />
-            </p> */}
+            <p className="admin-text">
+                Administrador
+                <Switch
+                    defaultChecked
+                    onChange={onChange}
+                    checked={isAdmin}
+                    className="admin-switch"
+                />
+            </p>
+            {}
+            <Modal
+                title="Adminisrar Admins"
+                visible={state.visible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+            >
+                <p>Desea cambiar los permisos del usuario: {nombre} </p>
+            </Modal>
         </Card>
     );
 };
